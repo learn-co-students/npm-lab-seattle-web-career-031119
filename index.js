da1fd8761f87f9bb007625ea091e7b86df00a804
github username <github email address>
2019-05-12 10:29:22 -0700

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function displayTime() {
       clock.style.color = '#'+Math.floor(Math.random()*16777215).toString(16);
       clock.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
   }, 1000)
-
-
 }
+
 
 window.onload = displayTime()
